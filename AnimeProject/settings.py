03d06399003a6fe27a8638b18686db28618dc4ac
MohammedKaif037 <kaifmohammed037@gmail.com>
2024-06-06 00:07:01 +0530

    ANI APP  updated

--- AnimeProject/settings.py
+++ AnimeProject/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AnimeApp',
-    'accounts',
- ]
+  ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
